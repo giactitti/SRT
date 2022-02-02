e859b8403f1ce62afa09e3d2b70d8d7f176e9348
Giacomo Titti <giacomotitti@gmail.com>
2022-02-02 16:03:07 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,29 +1,29 @@
 //generate table
 var geom=ee.Geometry.BBox(94.1329327392578,25.514028970276666,94.5339337158203,25.890203351903423);
-var MapUnit=geom.coveringGrid(geom.projection(),10000);
-Map.addLayer(table,{},'units');
+var MapUnit=geom.coveringGrid(geom.projection(),1000);
+Map.addLayer(MapUnit,{},'units');
 
 //import tool
 var SRT = require('users/giacomotitti/SRT:SRTfunctions');
 
 //temperature
-var temperature = SRT.SRTtemperature(MapUnit,2000,2021);//mapping unit, start date, end date
+var temperature = SRT.SRTtemperature(MapUnit,2000,2021,30);//mapping unit, start date, end date, scale
 print(temperature,'temperature');
 
-//geomorph
-var geomorphology = SRT.SRTgeomorphology(MapUnit,1000);//mapping unit, buffer radius for relief
-print(geomorphology,'geomorphology');
+geomorphometry
+var geomorphometry = SRT.SRTgeomorphometry(MapUnit,1000,30);//mapping unit, buffer radius for relief, scale
+print(geomorphometry,'geomorphometry');
 
 //geomorph
-var curvature = SRT.SRTcurvature(MapUnit);//mapping unit
+var curvature = SRT.SRTcurvature(MapUnit,30);//mapping unit, scale
 print(curvature,'curvature');
 
 // precipitation
-var precipitation = SRT.SRTprecipitation(MapUnit,1991,2020);//mapping unit, start date, end date
+var precipitation = SRT.SRTprecipitation(MapUnit,1991,2020,30);//mapping unit, start date, end date, scale
 print(precipitation,'precipitation');
 
 //ndvi
-var ndvi = SRT.SRTndvi(MapUnit,2011,2020);//mapping unit, start date, end date
+var ndvi = SRT.SRTndvi(MapUnit,2011,2020,30);//mapping unit, start date, end date, scale
 print(ndvi,'ndvi');
 
 //Display
@@ -32,8 +32,12 @@ var display = function(collection,name){
 var imaged = collection.reduceToImage({
 properties: [name],
 reducer: ee.Reducer.first()});
-Map.addLayer(imaged, {
-  min: -1,
+Map.addLayer(imaged,{},name)};
+
+display(ndvi,'NDVI_mean');
+//display(geomorphometry,'Slope');
+display(precipitation,'RnMax_mean');
+display(temperature,'TMax_mean');
   max: 1,
   palette: ['FCFDBF', 'FDAE78', 'EE605E', 'B63679', '711F81', '2C105C']
 },name)};
