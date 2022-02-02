fc6d92c0e38cb2b46d3d15ebdb30651d5ecf93cf
Giacomo Titti <giacomotitti@gmail.com>
2022-02-02 16:05:06 +0100

    Update SRTfunctions.js

--- SRTfunctions.js
+++ SRTfunctions.js
@@ -1,5 +1,5 @@
 /////////////////////////////////////////////////////////////reduction
-
+  
   var f = function(image,collection,scale) {
     return image.reduceRegions({
       collection: collection,
@@ -10,14 +10,14 @@
     scale: scale
     });
   };
-
+  
   var iter = function(var1,layer,collection,scale){
     var gridcoll=f(layer,collection,scale);
-
+    
     gridcoll = gridcoll.map(function(feature) {
       return feature.set(var1.concat('_mean'), feature.get('mean'));
     });
-
+    
     return gridcoll.map(function(feature) {
       return feature.set(var1.concat('_std'), feature.get('stdDev'));
     });
@@ -25,43 +25,44 @@
 
 /////////////////////////////////////////////////////////////////core function
 
-exports.SRTgeomorphology=function(table,RadiusInMeters){
+exports.SRTgeomorphometry=function(table,RadiusInMeters,scale){
   //Safanelli, J.L.; Poppiel, R.R.; Ruiz, L.F.C.; Bonfatti, B.R.; Mello, F.A.O.; Rizzo, R.; Demattê, J.A.M.
   //Terrain Analysis in Google Earth Engine: A Method Adapted for High-Performance Global-Scale Analysis.
   //ISPRS Int. J. Geo-Inf. 2020, 9, 400. DOI: https://doi.org/10.3390/ijgi9060400
   //GitHub: https://github.com/zecojls/tagee
   var TAGEE = require('users/joselucassafanelli/TAGEE:TAGEE-functions');
-
+  
   //var id=table2.filter(ee.Filter.eq("id",numero)).geometry();
   var id=table.geometry().bounds();//null,table.geometry().projection()););
   var gridcoll=ee.FeatureCollection(table).filterBounds(id);
-
+  
   /////////////////////////////////////////////////////////////topo
 
   var demSRTM1 = ee.Image('USGS/SRTMGL1_003');
   var demSRTM = demSRTM1.rename('DEM');
-  var DEMAttributes = TAGEE.terrainAnalysis(TAGEE, demSRTM, id);
-  var scale=DEMAttributes.select('Elevation').projection().nominalScale();
+  var DEMAttributes = TAGEE.terrainAnalysis(TAGEE, demSRTM, id,scale);
+  //var scale=DEMAttributes.select('Elevation').projection().nominalScale();
   //var maschera = DEMAttributes.select('Slope').gte(10);
   //var DEMAttributes = DEMAttributes.updateMask(maschera);
-
+  
   ///////////////////////////////////////////////////////////////relief
-
+  
   var reduced = DEMAttributes.select('Elevation').projection().atScale(scale);
   var dem= DEMAttributes.select('Elevation').reproject(reduced);
   var k1 = dem.focal_max(RadiusInMeters,'square','meters',1);
   var k2 = dem.focal_min(RadiusInMeters,'square','meters',1);//1km di raggio
   var range = k1.subtract(k2);
-
+  
   ////////////////////////////////////////////////////////////slope, hcv, vcv
   var gridcoll = iter('Slope',DEMAttributes.select('Slope'),gridcoll,scale);
   var gridcoll = iter('Elev',DEMAttributes.select('Elevation'),gridcoll,scale);
   var gridcoll = iter('Asp',DEMAttributes.select('Aspect'),gridcoll,scale);
+  var gridcoll = iter('Hill',DEMAttributes.select('Hillshade'),gridcoll,scale);
   var gridcoll = iter('Nss',DEMAttributes.select('Northness'),gridcoll,scale);
   var gridcoll = iter('Ess',DEMAttributes.select('Eastness'),gridcoll,scale);
   var gridcoll = iter('SIdx',DEMAttributes.select('ShapeIndex'),gridcoll,scale);
   var gridcoll = iter('Rlf',range,gridcoll,scale);
-
+  
   /*var gridcoll = iter('MCv',DEMAttributes.select('MeanCurvature'),gridcoll);
   var gridcoll = iter('GCv',DEMAttributes.select('GaussianCurvature'),gridcoll);
   var gridcoll = iter('MinCv',DEMAttributes.select('MinimalCurvature'),gridcoll);
@@ -71,27 +72,26 @@ exports.SRTgeomorphology=function(table,RadiusInMeters){
   return gridcoll;
 };
 
-exports.SRTcurvature=function(table){
+exports.SRTcurvature=function(table,scale){
   //Safanelli, J.L.; Poppiel, R.R.; Ruiz, L.F.C.; Bonfatti, B.R.; Mello, F.A.O.; Rizzo, R.; Demattê, J.A.M.
   //Terrain Analysis in Google Earth Engine: A Method Adapted for High-Performance Global-Scale Analysis.
   //ISPRS Int. J. Geo-Inf. 2020, 9, 400. DOI: https://doi.org/10.3390/ijgi9060400
   //GitHub: https://github.com/zecojls/tagee
   var TAGEE = require('users/joselucassafanelli/TAGEE:TAGEE-functions');
-
+  
   //var id=table2.filter(ee.Filter.eq("id",numero)).geometry();
   var id=table.geometry().bounds();//null,table.geometry().projection());
   var gridcoll=ee.FeatureCollection(table).filterBounds(id);
-
+  
   /////////////////////////////////////////////////////////////topo
 
   var demSRTM1 = ee.Image('USGS/SRTMGL1_003');
   var demSRTM = demSRTM1.rename('DEM');
   var DEMAttributes = TAGEE.terrainAnalysis(TAGEE, demSRTM, id);
-  var scale=DEMAttributes.select('Elevation').projection().nominalScale();
+  //var scale=DEMAttributes.select('Elevation').projection().nominalScale();
   //var maschera = DEMAttributes.select('Slope').gte(10);
   //var DEMAttributes = DEMAttributes.updateMask(maschera);
-
-
+  
   ////////////////////////////////////////////////////////////curvature
   var gridcoll = iter('MCv',DEMAttributes.select('MeanCurvature'),gridcoll,scale);
   var gridcoll = iter('GCv',DEMAttributes.select('GaussianCurvature'),gridcoll,scale);
@@ -104,15 +104,15 @@ exports.SRTcurvature=function(table){
 
 ///////////////////////////////////////////////////////////////core function
 
-exports.SRTprecipitation=function(table,start,end){
+exports.SRTprecipitation=function(table,start,end,scale){
   var id=table.geometry();//.bounds(table.geometry().projection());
   var gridcoll=ee.FeatureCollection(table).filterBounds(id);
 
   /////////////////////////////////////////////////////////////////precipitation
-
+  
   var pioggia=ee.ImageCollection("UCSB-CHG/CHIRPS/DAILY").filterBounds(id);
   var years = ee.List.sequence(start, end);
-  var precip = years.map(function(m) {
+  var precip = years.map(function(m) { 
   var precip_y = pioggia.filter(ee.Filter.calendarRange(m, m, 'year'));
   var precip_x = precip_y.reduce({
         reducer: ee.Reducer.sum().combine({
@@ -129,10 +129,8 @@ exports.SRTprecipitation=function(table,start,end){
   });
   var precip_sum = ee.ImageCollection(precip);
   var cumPrec = precip_sum.reduce(ee.Reducer.median()).resample('bicubic');
-  var scale=cumPrec.select('precipitation_sum_median').projection().nominalScale();
-
-
-
+  //var scale=cumPrec.select('precipitation_sum_median').projection().nominalScale();
+  
   /////////////////////////////////////////////////////////////////reduction
   var gridcoll = iter('RnSum',cumPrec.select('precipitation_sum_median'),gridcoll,scale);
   var gridcoll = iter('RnMax',cumPrec.select('precipitation_max_median'),gridcoll,scale);
@@ -142,16 +140,16 @@ exports.SRTprecipitation=function(table,start,end){
 };
 
 ///////////////////////////////////////////////////////////////////core function
-exports.SRTtemperature=function(table,start,end){
+exports.SRTtemperature=function(table,start,end,scale){
   var id=table.geometry();//.bounds(table.geometry().projection());
-  var gridcoll=ee.FeatureCollection(table).filterBounds(id);
-
+  var gridcoll=ee.FeatureCollection(table).filterBounds(id);  
+  
   ////////////////////////////////////////////////////////////Temp
-
+  
   var dataset=ee.ImageCollection("MODIS/006/MOD11A1").filterBounds(id);
   var landSurfaceTemperature = dataset.select('LST_Day_1km');
   var yearst = ee.List.sequence(start, end);
-  var temp = yearst.map(function(m) {
+  var temp = yearst.map(function(m) { 
   var temp_y = landSurfaceTemperature.filter(ee.Filter.calendarRange(m, m, 'year'));
   var temp_x = temp_y.reduce({
         reducer: ee.Reducer.median().combine({
@@ -166,10 +164,10 @@ exports.SRTtemperature=function(table,start,end){
   });
   var temp_sum = ee.ImageCollection(temp);
   var cumTemp = temp_sum.reduce(ee.Reducer.median()).resample('bicubic');
-  var scale=cumTemp.select('LST_Day_1km_median_median').projection().nominalScale();
-
+  //var scale=cumTemp.select('LST_Day_1km_median_median').projection().nominalScale();
+  
   //print(cumTemp,'cum')
-
+  
   var gridcoll = iter('TMed',cumTemp.select('LST_Day_1km_median_median'),gridcoll,scale);
   var gridcoll = iter('TStd',cumTemp.select('LST_Day_1km_stdDev_median'),gridcoll,scale);
   var gridcoll = iter('TMax',cumTemp.select('LST_Day_1km_max_median'),gridcoll,scale);
@@ -178,12 +176,12 @@ exports.SRTtemperature=function(table,start,end){
 
 
 ////////////////////////////////////////////////////////////////////core function
-exports.SRTndvi=function(table,start,end){
+exports.SRTndvi=function(table,start,end,scale){
   var id=table.geometry();//.bounds(table.geometry().projection());
-  var gridcoll=ee.FeatureCollection(table).filterBounds(id);
-
+  var gridcoll=ee.FeatureCollection(table).filterBounds(id);  
+  
   /////////////////////////////////////////////////////////////////NDVI Sentinel 2
-
+  
   // Create image collection of S-2 imagery for the perdiod 2016-2018
   var S2 = ee.ImageCollection('COPERNICUS/S2')
   //filter start and end date
@@ -203,17 +201,16 @@ exports.SRTndvi=function(table,start,end){
   // Extract NDVI band and create NDVI median composite image
   var NDVIs = S2.select(['nd']);
   var NDVImed = NDVIs.reduce(ee.Reducer.median());
-
+  
   ////////////////////////////////////////////////////////////NDVI Landsat 7
   var a=start
   var b=end
   var datasetndvi = ee.ImageCollection('LANDSAT/LE07/C01/T1_32DAY_NDVI')
   .filterDate(a.toString(), b.toString()).filterBounds(id);
   var NDVI = datasetndvi.reduce(ee.Reducer.median());
-  var scale=NDVI.select('NDVI_median').projection().nominalScale();
-  //print(NDVI)
+  //var scale=NDVI.select('NDVI_median').projection().nominalScale();
   //Map.addLayer(NDVI,{},'ndvi')
-
+  
   //var L8 = ee.ImageCollection("LANDSAT/LE07/C01/T1_TOA")
   //.filterDate('2011-01-01', '2020-12-31').filterBounds(id);
 
@@ -230,10 +227,10 @@ exports.SRTndvi=function(table,start,end){
   // Return the masked image with an NDVI band.
   //return image.addBands(ndvi).updateMask(mask);
   //});
-
+  
   //var NDVI = cloudlessNDVI.reduce(ee.Reducer.median());
   //print(NDVI)
-
+  
   //////////////////////////////////////////////////////////////////////reduction
   var gridcoll = iter('NDVI',NDVI.select('NDVI_median'),gridcoll,scale);//landsat*/
   //var gridcoll = iter('NDVI',NDVImed.select('nd'));//sentinel
